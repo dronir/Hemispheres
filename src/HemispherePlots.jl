f19702921c978f6199ac5068da289d2d39db217b
Olli Wilkman <olli.wilkman@iki.fi>
2015-12-08 18:08:35 +0200

    Update HemispherePlots to work at least to some extent

--- src/HemispherePlots.jl
+++ src/HemispherePlots.jl
@@ -1,13 +1,15 @@
 module HemispherePlots
 
-using Hemispheres	
-using Pyplot
+using ScatteringLaws
+using PyCall
+
+@pyimport matplotlib.pyplot as plot
 
 
 # This function makes a plot of a given hemisphere.
-plot_hemisphere(H::Hemisphere, thetaI::Real) = plot_hemisphere(H,thetaI,"",[])
-plot_hemisphere(H::Hemisphere, thetaI::Real, saveplot::String) = plot_hemisphere(H,thetaI,saveplot,[])
-function plot_hemisphere(H::Hemisphere, thetaI::Real, saveplot::String, vrange::Array)
+plot_hemisphere(H::ScatteringLaw, thetaI::Real) = plot_hemisphere(H,thetaI,"",[])
+plot_hemisphere(H::ScatteringLaw, thetaI::Real, saveplot::String) = plot_hemisphere(H,thetaI,saveplot,[])
+function plot_hemisphere(H::ScatteringLaw, thetaI::Real, saveplot::String, vrange::Array)
     patches = Any[]
 	const DEG = 57.295791433
     N = H.nTheta
@@ -54,20 +56,25 @@ end
 
 # Make a plot of the primary plane
 # (theta_e varies from -85 to +85 with theta_i constant)
-function get_primary_plane(H::Hemisphere, thetaI::Real, N::Integer)
+function get_primary_plane(H::ScatteringLaw, thetaI::Real, N::Integer)
 	X = linspace(-85.0, 85.0, N)
 	Y = zeros(N)
 	for i = 1:N
 		x = X[i]
 		theta_e = abs(x)*pi/180
-		t = int(fld(theta_e, H.dTheta))+1
-		phi = x<=0 ? 10*eps() : pi-0.5*H.dPhi[t]
+		if typeof(H) == Hemisphere
+			t = int(fld(theta_e, H.dTheta))+1
+			phi = x<=0 ? 10*eps() : pi-0.5*H.dPhi[t]
+		else
+			phi = x<=0 ? 10*eps() : pi-0.001
+		end
 		G = Geometry(thetaI, theta_e, phi)
 		Y[i] = value(H,G)
 	end
 	return X,Y
 end
-function plot_primary_plane(H::Hemisphere, thetaI::Real, N::Integer)
+
+function plot_primary_plane(H::ScatteringLaw, thetaI::Real, N::Integer)
 	X,Y = get_primary_plane(H,thetaI,N)
 	plot.plot(X,Y)
 	plot.xlim(-90,90)
@@ -75,9 +82,19 @@ function plot_primary_plane(H::Hemisphere, thetaI::Real, N::Integer)
 	plot.show()
 end
 
+function plot_primary_plane{T<:ScatteringLaw}(A::Array{T,1}, thetaI::Real, N::Integer)
+	for H in A
+		X,Y = get_primary_plane(H,thetaI,N)
+		plot.plot(X,Y)
+	end
+	plot.xlim(-90,90)
+	plot.axvline(0,color="black")
+	plot.show()
+end
+
 # Make a plot of the emergent plane
 # (theta_i varies from -85 to +85 with theta_e constant)
-function plot_emergent_plane(H::Hemisphere, theta_e::Real, N::Integer)
+function plot_emergent_plane(H::ScatteringLaw, theta_e::Real, N::Integer)
 	X = linspace(-85.0, 85.0, N)
 	Y = zeros(N)
 	for i = 1:N
@@ -96,7 +113,7 @@ end
 
 # Make a plot of the disk
 # (theta_i and theta_e vary from -85 to +85 with alpha constant)
-function plot_disk(H::Hemisphere, alpha::Real, N::Integer)
+function plot_disk(H::ScatteringLaw, alpha::Real, N::Integer)
     alpha = alpha * 180/pi
 	X = linspace(-85.0+alpha, 85.0, N)
 	Y = zeros(N)
